9c5c628ec38bdabb2f71ffbb2623ac80417510b0
Solomon Jin <solomonjin135@yahoo.com>
2021-05-10 12:28:57 -0700

    Completed javascript-operators-and-expressions

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 5;
+var height = 12;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 42;
+var payment = 58;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 83;
+var midterm = 77;
+var final = 95;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Solomon';
+var lastName = 'Jin';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 6;
+var isAcidic = pH < 7;
+console.log('value of pH:', isAcidic);
+console.log('typeof pH:', typeof isAcidic);
+
+var headCount = 9;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' is the GOAT';
+console.log('value of:', motto);
+console.log('typeof motto:', typeof motto);
