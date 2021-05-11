8c9a9fcf30ee83ef1ffe0be69cb329d68a586970
Solomon Jin <solomonjin135@yahoo.com>
2021-05-11 11:09:29 -0700

    Added data for delivery status/date, delivery recipient as well as img source

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,11 +3,19 @@ var orderHistory = [
     order_num: '114-3941689-8772232',
     total: 34.00,
     date: 'August 4, 2020',
+    recipient: 'JS Masher',
+    delivered: {
+      status: true,
+      deliverDate: 'Aug 8, 2020'
+    },
     items: [
       {
         name: 'JavaScript for impatient programmers',
         price: 31.55,
-        more_info: 'Rauschmayer, Dr. Axel'
+        moreInfo: {
+          author: 'Rauschmayer, Dr. Axel',
+          image: 'url'
+        }
       }
     ]
   },
@@ -16,11 +24,19 @@ var orderHistory = [
     order_num: '113-9984268-1280257',
     total: 44.53,
     date: 'July 19, 2020',
+    recipient: 'JS Masher',
+    delivered: {
+      status: true,
+      deliverDate: 'Jul 20, 2020'
+    },
     items: [
       {
         name: 'The Timeless Way of Building',
         price: 41.53,
-        more_info: 'Alexander, Christopher'
+        moreInfo: {
+          author: 'Alexander, Christopher',
+          image: 'url'
+        }
       }
     ]
   },
@@ -29,11 +45,18 @@ var orderHistory = [
     order_num: '114-2875557-905409',
     total: 17.22,
     date: 'July 4, 2020',
+    recipient: 'JS Masher',
+    delivered: {
+      status: true,
+      deliverDate: 'Jul 7, 2020'
+    },
     items: [
       {
         name: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
         price: 15.98,
-        more_info: ''
+        moreInfo: {
+          image: 'url'
+        }
       }
     ]
   },
@@ -42,17 +65,27 @@ var orderHistory = [
     order_num: '113-2883177-268248',
     total: 138.93,
     date: 'July 3, 2020',
+    recipient: 'JS Masher',
+    delivered: {
+      status: true,
+      deliverDate: 'Jul 5, 2020'
+    },
     items: [
       {
         name: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         price: 94.95,
-        more_info: ''
+        moreInfo: {
+          image: 'url'
+        }
       },
 
       {
         name: 'The Art of Sql',
         price: 33.99,
-        more_info: 'Faroult, Stephane'
+        moreInfo: {
+          author: 'Faroult, Stephane',
+          image: 'url'
+        }
       }
     ]
   }
