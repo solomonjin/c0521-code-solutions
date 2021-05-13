83f973b145901323a98bc34f0a4a719b918bac04
Solomon Jin <solomonjin135@yahoo.com>
2021-05-13 11:17:15 -0700

    Completed javascript-loops-practice

---
+++ javascript-loops-practice/get-indexes.js
@@ -0,0 +1,9 @@
+/* exported getIndexes */
+
+function getIndexes(array) {
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    result.push(i);
+  }
+  return result;
+}
---
+++ javascript-loops-practice/odd-or-even.js
@@ -0,0 +1,11 @@
+/* exported oddOrEven */
+
+function oddOrEven(numbers) {
+  var result = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      result.push('even');
+    } else result.push('odd');
+  }
+  return result;
+}
