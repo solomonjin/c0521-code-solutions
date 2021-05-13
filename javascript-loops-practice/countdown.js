8e521133c333ce859e5938d4d40b5b0e73782c39
Solomon Jin <solomonjin135@yahoo.com>
2021-05-13 11:25:55 -0700

    Changed typeof 'string' to 'string' and switched to decrement operator

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -2,7 +2,7 @@
 
 function countdown(number) {
   var result = [];
-  for (var i = number; i >= 0; i = i - 1) {
+  for (var i = number; i >= 0; i--) {
     result.push(i);
   }
   return result;
--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -3,7 +3,7 @@
 function filterOutStrings(values) {
   var noStrings = [];
   for (var i = 0; i < values.length; i++) {
-    if (typeof values[i] !== typeof 'string') {
+    if (typeof values[i] !== 'string') {
       noStrings.push(values[i]);
     }
   }
