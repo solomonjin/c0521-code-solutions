8df944dc756dacb8bd7875478dc56bf6bc79e1d7
Solomon Jin <solomonjin135@yahoo.com>
2021-05-17 16:34:36 -0700

    Changed conditionals to if else

--- dom-manipulation/main.js
+++ dom-manipulation/main.js
@@ -6,9 +6,9 @@ var $clickCount = document.querySelector('.click-count');
 $hotButton.addEventListener('click', function (event) {
   clickCount++;
   $clickCount.textContent = clickCount;
-  if (clickCount >= 4) $hotButton.className = 'hot-button cool';
-  if (clickCount >= 7) $hotButton.className = 'hot-button tepid';
-  if (clickCount >= 10) $hotButton.className = 'hot-button warm';
-  if (clickCount >= 13) $hotButton.className = 'hot-button hot';
   if (clickCount >= 16) $hotButton.className = 'hot-button nuclear';
+  else if (clickCount >= 13) $hotButton.className = 'hot-button hot';
+  else if (clickCount >= 10) $hotButton.className = 'hot-button warm';
+  else if (clickCount >= 7) $hotButton.className = 'hot-button tepid';
+  else if (clickCount >= 4) $hotButton.className = 'hot-button cool';
 });
