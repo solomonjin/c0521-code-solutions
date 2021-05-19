21f8cd37b1c94dae9fe8d467aa65271649115bf9
Solomon Jin <solomonjin135@yahoo.com>
2021-05-19 15:13:19 -0700

    Completed dom-creation

---
+++ dom-creation/main.js
@@ -0,0 +1,88 @@
+/* exported pokedex */
+
+var pokedex = [
+  {
+    number: '001',
+    name: 'Bulbasaur',
+    description: 'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+    imageUrl: 'images/bulbasaur.png'
+  },
+  {
+    number: '004',
+    name: 'Charmander',
+    description: 'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+    imageUrl: 'images/charmander.png'
+  },
+  {
+    number: '007',
+    name: 'Squirtle',
+    description: 'When it retracts its long neck into its shell, it squirts out water with vigorous force.',
+    imageUrl: 'images/squirtle.png'
+  },
+  {
+    number: '002',
+    name: 'Ivysaur',
+    description: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+    imageUrl: 'images/ivysaur.png'
+  },
+  {
+    number: '005',
+    name: 'Charmeleon',
+    description: 'It has a barbaric nature. In battle, it whips its fiery tail around and slashes away with sharp claws.',
+    imageUrl: 'images/charmeleon.png'
+  },
+  {
+    number: '008',
+    name: 'Wartortle',
+    description: 'It is recognized as a symbol of longevity. If its shell has algae on it, that Wartortle is very old.',
+    imageUrl: 'images/wartortle.png'
+  },
+  {
+    number: '003',
+    name: 'Venusaur',
+    description: 'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+    imageUrl: 'images/venusaur.png'
+  },
+  {
+    number: '006',
+    name: 'Charizard',
+    description: 'It spits fire that is hot enough to melt boulders. It may cause forest fires by blowing flames.',
+    imageUrl: 'images/charizard.png'
+  },
+  {
+    number: '009',
+    name: 'Blastoise',
+    description: 'It crushes its foe under its heavy body to cause fainting. In a pinch, it will withdraw inside its shell.',
+    imageUrl: 'images/blastoise.png'
+  }
+];
+
+function renderPokemon(pokemon) {
+  var newPokemon = document.createElement('div');
+  newPokemon.setAttribute('class', 'column-third');
+  var newCard = document.createElement('div');
+  newCard.setAttribute('class', 'pokemon-card');
+  newPokemon.appendChild(newCard);
+  var newImage = document.createElement('img');
+  var aboutPokemon = document.createElement('div');
+  newImage.setAttribute('src', pokemon.imageUrl);
+  aboutPokemon.setAttribute('class', 'pokemon-card-text');
+  newCard.appendChild(newImage);
+  var pokemonName = document.createElement('h2');
+  pokemonName.textContent = pokemon.name;
+  var pokemonNum = document.createElement('h3');
+  pokemonNum.textContent = pokemon.number;
+  var pokemonText = document.createElement('p');
+  pokemonText.textContent = pokemon.description;
+  aboutPokemon.appendChild(pokemonName);
+  aboutPokemon.appendChild(pokemonNum);
+  aboutPokemon.appendChild(pokemonText);
+  newCard.appendChild(aboutPokemon);
+  return newPokemon;
+}
+
+var $pokemonCollection = document.querySelector('.row');
+
+for (var i = 0; i < pokedex.length; i++) {
+  $pokemonCollection.appendChild(renderPokemon(pokedex[i]));
+}
