47386d8949be7453261fd98ead31d61619850e93
Solomon Jin <solomonjin135@yahoo.com>
2021-05-21 14:41:54 -0700

    Completed javascript-local-storage

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,12 @@
+/* exported todos */
+
+var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON) todos = JSON.parse(previousTodosJSON);
+
+window.addEventListener('beforeunload', handleUnload);
+
+function handleUnload(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
