a34855605d9356333a546d92277211ac8562f4b8
Solomon Jin <solomonjin135@yahoo.com>
2021-06-24 01:02:16 +0000

    Completed bonus question

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 
-const fileList = process.argv.filter((x, i) => i > 1).map((x, i) => ({ title: x, index: i }));
+const fileList = process.argv.filter((x, i) => i > 1);
 
 const textList = [];
 
 for (let i = 0; i < fileList.length; i++) {
-  fs.readFile(fileList[i].title, 'utf8', (err, data) => {
+  fs.readFile(fileList[i], 'utf8', (err, data) => {
     if (err) throw err;
-    textList.push(data);
-    if (textList.length === fileList.length) console.log(textList);
+    textList.push({ text: data, index: i });
+    if (textList.length === fileList.length) printFiles();
   });
 }
 
 function printFiles() {
-  textList.forEach(text => console.log(text));
+  for (let i = 0; i < textList.length; i++) {
+    console.log(textList.find(text => text.index === i).text);
+  }
 }
