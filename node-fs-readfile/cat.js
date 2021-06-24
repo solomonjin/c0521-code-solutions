b52c484076a16ff3793e18721d284b21cf239779
Solomon Jin <solomonjin135@yahoo.com>
2021-06-24 00:46:57 +0000

    progress

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -2,4 +2,16 @@ const fs = require('fs');
 
 const fileList = process.argv.filter((x, i) => i > 1).map((x, i) => ({ title: x, index: i }));
 
-console.log(fileList);
+const textList = [];
+
+for (let i = 0; i < fileList.length; i++) {
+  fs.readFile(fileList[i].title, 'utf8', (err, data) => {
+    if (err) throw err;
+    textList.push(data);
+    if (textList.length === fileList.length) console.log(textList);
+  });
+}
+
+function printFiles() {
+  textList.forEach(text => console.log(text));
+}
