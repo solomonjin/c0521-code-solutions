3646b977d1200a89c263d2078b67e16e3f047079
Solomon Jin <solomonjin135@yahoo.com>
2021-06-25 21:54:23 +0000

    Completed express-intro

---
+++ express-intro/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+
+const app = express();
+
+// eslint-disable-next-line no-console
+app.listen(3000, () => { console.log('Express server listening on port 3000'); });
