633907df272ef585a70845947681ae8849784ba1
Solomon Jin <solomonjin135@yahoo.com>
2021-07-02 00:59:14 +0000

    Completed sgt-back-end

--- sgt-back-end/index.js
+++ sgt-back-end/index.js
@@ -54,4 +54,75 @@ app.post('/api/grades', (req, res) => {
     });
 });
 
+app.put('/api/grades/:gradeId', (req, res) => {
+  const gradeId = parseFloat(req.params.gradeId);
+  if (gradeId <= 0 || !Number.isInteger(gradeId)) {
+    res.status(400).json({ error: `The given gradeId, ${gradeId} is not valid.` });
+    return;
+  }
+
+  const { name, course, score } = req.body;
+  if (!name || !course || !score) {
+    res.status(400).json({ error: 'name, course, or score value(s) are missing' });
+    return;
+  }
+
+  const inputScore = parseFloat(score);
+  if (!Number.isInteger(inputScore) || inputScore < 0 || inputScore > 100) {
+    res.status(400).json({ error: `score ${inputScore} is not a valid integer.` });
+    return;
+  }
+
+  const sql = `
+  update "grades"
+     set "name" = $1,
+         "course" = $2,
+         "score" = $3
+   where "gradeId" = $4
+   returning *
+  `;
+  const params = [name, course, score, gradeId];
+
+  db.query(sql, params)
+    .then(result => {
+      if (result.rows.length === 0) {
+        res.status(404).json({ error: `Cannot find grade with 'gradeId' ${gradeId}` });
+      } else {
+        res.json(result.rows[0]);
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+});
+
+app.delete('/api/grades/:gradeId', (req, res) => {
+  const gradeId = parseFloat(req.params.gradeId);
+  if (gradeId <= 0 || !Number.isInteger(gradeId)) {
+    res.status(400).json({ error: `The given gradeId, ${gradeId} is not valid.` });
+    return;
+  }
+
+  const sql = `
+  delete from "grades"
+        where "gradeId" = $1
+    returning *
+  `;
+  const params = [gradeId];
+
+  db.query(sql, params)
+    .then(result => {
+      if (result.rows.length === 0) {
+        res.status(404).json({ error: `Cannot find grade with 'gradeId' ${gradeId}` });
+      } else {
+        res.status(204).json();
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'An unexpected error occurred.' });
+    });
+});
+
 app.listen(3000);
