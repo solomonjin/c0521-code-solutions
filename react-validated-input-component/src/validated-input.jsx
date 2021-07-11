4f8a758f6105743d5f47306bfa907345ce816d62
Solomon Jin <solomonjin135@yahoo.com>
2021-07-11 15:11:44 -0700

    Completed react-validated-input-component

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -12,13 +12,20 @@ class ValidatedInput extends React.Component {
   }
 
   render() {
-    const validSymbol = this.state.value.length <= 8
+    const validPW = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()])/.test(this.state.value);
+    const validSymbol = this.state.value.length <= 8 || !validPW
       ? <i className="fas fa-times red" />
       : <i className="fas fa-check green" />;
 
-    const errorMessage = this.state.value.length === 0
-      ? 'A password is required.'
-      : 'Your password is too short.';
+    let errorMessage = '';
+    if (!validPW) {
+      errorMessage =
+    `Password requires one uppercase
+    and lowercase letters, a digit,
+    and a special character.`;
+    }
+    if (this.state.value.length < 8) errorMessage = 'Your password is too short.';
+    if (this.state.value.length === 0) errorMessage = 'A password is required.';
 
     return (
     <form>
@@ -29,7 +36,7 @@ class ValidatedInput extends React.Component {
         <input id="user-password" type="password" value={this.state.value} onChange={this.handleChange} />
         {validSymbol}
       </div>
-        {this.state.value.length <= 8 &&
+        {(this.state.value.length <= 8 || !validPW) &&
         <div className="row">
           <span className="red">{errorMessage}</span>
         </div>}
