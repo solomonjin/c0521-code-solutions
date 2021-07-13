511513a651af69863af7c005bd8b57b012804b26
Solomon Jin <solomonjin135@yahoo.com>
2021-07-12 21:45:33 -0700

    Completed base functionality of carousel.

--- react-carousel-component/src/carousel.jsx
+++ react-carousel-component/src/carousel.jsx
@@ -3,17 +3,28 @@ import React from 'react';
 class Carousel extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { currentImg: 0 };
+    this.state = { currentIndex: 0 };
     this.previousImg = this.previousImg.bind(this);
     this.nextImg = this.nextImg.bind(this);
   }
 
-  render() {
-    const imageDots = [<i key="0" className="fas fa-circle dot" />];
-    for (let i = 1; i < this.props.images.length; i++) {
-      imageDots.push(<i key={i.toString()} className="far fa-circle dot" />);
+  nextImg() {
+    if (this.state.currentIndex + 1 > this.props.images.length - 1) {
+      this.setState({ currentIndex: 0 });
+    } else {
+      this.setState({ currentIndex: this.state.currentIndex + 1 });
     }
+  }
 
+  previousImg() {
+    if (this.state.currentIndex - 1 < 0) {
+      this.setState({ currentIndex: this.props.images.length - 1 });
+    } else {
+      this.setState({ currentIndex: this.state.currentIndex - 1 });
+    }
+  }
+
+  render() {
     return (
       <div className="container">
         <div className="row">
@@ -21,18 +32,38 @@ class Carousel extends React.Component {
             <i className="fas fa-chevron-left arrow" onClick={this.previousImg} />
           </div>
           <div className="col-60">
-            <img src={this.props.images[0]} />
+            <Images currentIndex={this.state.currentIndex} images={this.props.images} />
           </div>
           <div className="col-20">
             <i className="fas fa-chevron-right arrow" onClick={this.nextImg} />
           </div>
         </div>
         <div className="row">
-          {imageDots}
+          <ImageDots length={this.props.images.length} currentIndex={this.state.currentIndex} />
         </div>
       </div>
     );
   }
+}
+
+class Images extends React.Component {
+  render() {
+    return (
+      <>
+        {this.props.images.map((img, index) => <img key={index} src={img}
+                                className={index === this.props.currentIndex ? 'show' : ''} />)}
+      </>
+    );
+  }
+}
+
+function ImageDots(props) {
+  const imageDots = [];
+  for (let i = 0; i < props.length; i++) {
+    const dotClass = i === props.currentIndex ? 'fas fa-circle dot' : 'far fa-circle dot';
+    imageDots.push(<i key={i.toString()} className={dotClass} />);
+  }
+  return imageDots;
 }
 
 export default Carousel;
