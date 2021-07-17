0a6216a961b2ca074d5fd74a58a4c559cb42e802
Solomon Jin <solomonjin135@yahoo.com>
2021-07-17 00:52:11 +0000

    Refactored stopwatch into using React hooks

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -1,71 +1,33 @@
 import React, { useState } from 'react';
 
-// class Stopwatch extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isPlaying: false,
-//       count: 0
-//     };
-//     this.togglePlay = this.togglePlay.bind(this);
-//     this.clickTimer = this.clickTimer.bind(this);
-//   }
-
-//   togglePlay() {
-//     this.setState({ isPlaying: !this.state.isPlaying });
-//     if (this.timerID) {
-//       clearInterval(this.timerID);
-//       this.timerID = null;
-//     } else {
-//       this.timerID = setInterval(() => {
-//         this.setState({ count: this.state.count + 1 });
-//       }, 1000);
-//     }
-//   }
-
-//   clickTimer() {
-//     if (this.state.isPlaying) return;
-
-//     this.setState({ count: 0 });
-//   }
-
-//   render() {
-//     return (
-//       <div className="row">
-//         <div className="timer" onClick={this.clickTimer}>
-//           <span>{this.state.count}</span>
-//         </div>
-//         <i onClick={this.togglePlay} className={this.state.isPlaying ? 'fas fa-pause' : 'fas fa-play'} />
-//       </div>
-//     );
-//   }
-// }
-
 function Stopwatch(props) {
-  const [timer, setTimer] = useState({
-    isPlaying: false,
-    count: 0,
-    timerID: null
-  });
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [timer, setTimer] = useState(null);
+  const [count, setCount] = useState(0);
 
   const togglePlay = () => {
-    let newTimer;
-    if (timer.timerID) {
-      clearInterval(timer.timerID);
-      newTimer = null;
+    if (timer) {
+      clearInterval(timer);
+      setTimer(null);
     } else {
-      newTimer = setInterval(() => {
-        setTimer({ ...timer, count: timer.count + 1 });
-      }, 1000);
+      setTimer(setInterval(() => setCount(count => count + 1), 1000));
     }
-    setTimer({ ...timer, isPlaying: !timer.isPlaying, timerID: newTimer });
+    setIsPlaying(!isPlaying);
   };
 
   const clickTimer = () => {
-    if (timer.isPlaying) {
-      setTimer();
-    }
-  };
-}
+    if (isPlaying) return;
 
+    setCount(0);
+  };
+
+  return (
+    <div className="row">
+      <div className="timer" onClick={clickTimer}>
+        <span>{count}</span>
+      </div>
+      <i onClick={togglePlay} className={isPlaying ? 'fas fa-pause' : 'fas fa-play'} />
+    </div>
+  );
+}
 export { Stopwatch };
